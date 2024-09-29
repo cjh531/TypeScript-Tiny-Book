d715399721910d4029508167de6fa6f546461cac
chen junheng <13688939119@163.com>
2024-09-29 14:33:42 +0800

    feat: 练习 double-assert

--- packages/04-any-unknown-never/src/double-assert.ts
+++ packages/04-any-unknown-never/src/double-assert.ts
@@ -1,4 +1,10 @@
-const str: string = 'linbudu';
+// const str: string = 'linbudu';
+
+// (str as unknown as { handler: () => {} }).handler();
+// // 使用尖括号断言
+// (<{ handler: () => {} }>(<unknown>str)).handler();
+
+const str: string = 'cjh';
 
 (str as unknown as { handler: () => {} }).handler();
 // 使用尖括号断言
